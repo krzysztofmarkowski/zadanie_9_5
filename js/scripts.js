0aadd2b6d90b48a38ddc504e2b34d2f1665469ba
Krzysztof Markowski <markowski@gdansk.home.pl>
2017-07-07 20:49:59 +0200

    Use of the metod getElementsByClassName and properties innerText

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
- 
+var getElementsButtonClass = document.getElementsByClassName('button');
+console.log(getElementsButtonClass);
+console.log(getElementsButtonClass.length);
+for (var i = 0; i < getElementsButtonClass.length; i++) {
+    button_nr = i + 1;
+    console.log('Text on button number ' + button_nr + ': ' + getElementsButtonClass[i].innerText);
+}
